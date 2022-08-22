57bca8034ab1b9678f753212c1eecea29fa0768c
Kra1o5 <kra1o5x@gmail.com>
2022-08-22 20:22:04 +0000

    OrchidOs overlay: Introduce Immersive Navigation [3/3]
    Based in Hide gesture navbar from Syberia project
    
    Change-Id: I805df9d44920b0274f8495820e401cb1bac1e16b
    Co-Authored-By: DennySPb <dennyspb@gmail.com>
    Co-Authored-By: Víctor Santoja <victorsantoja@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -227,3 +227,5 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+
+-include vendor/lineage/overlay/overlays.mk
